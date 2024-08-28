fb0f3a674610e73f17b9f0ad2cde10de01d767c1
khushboo <palkhushboo2632@gmail.com>
2024-08-28 16:35:02 +0530

    connected to the database

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+
+export const signupUser=async(req,res)=>{
+try{
+  const{fullname,username,password,} =req.body;
+}catch(error){
+
+}
+}
+export const loginUser=(req,res)=>{
+ 
+ console.log("login user");
+}
+export const logoutUser=(req,res)=>{
+ console.log("login user");
+}
