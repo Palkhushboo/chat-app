0ed07c934171d2373f2e15d2e58fa3624305aee9
khushboo <palkhushboo2632@gmail.com>
2024-08-28 23:25:36 +0530

    set cookie

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connecttomongodbfile.js";
-const app=express();
-dotenv.config();
-const PORT=process.env.PORT || 5000;
-app.get("/",(req,res)=>{
- res.send("hello world //")
-})
 
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
 app.use("/api/auth",authRoutes);
+
 app.listen(PORT,()=>{
  connectToMongoDB();
  console.log(`server runing on port ${PORT}`)
