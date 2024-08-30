6919a204a8a2a9c02dd04e34017ab26f32e2208c
khushboo <palkhushboo2632@gmail.com>
2024-08-30 18:01:23 +0530

    get messages endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -47,42 +47,23 @@ export const sendMessage = async (req, res) => {
   }
 };
 
-// import mongoose from "mongoose";
-// import Conversation from "../models/conversation.model.js"
-// import Message from "../models/message.mongoose.js";
-// export const sendMessage=async(req,res)=>{
-//  try {
-//   const {message}=req.body;
-//   console.log(req.params)
-//   console.log(req.user._id);
-//   const {id:receiverId}=req.params;
-//   const senderId=req.user._id;
-//   //console.log(receiverId,senderId);
-//   // Convert senderId and receiverId to ObjectId instances
-  
+export const getMessage=async(req,res)=>{
+  try {
+    const{id:userToChatId}=req.params;
+    const senderId=req.user._id;
 
-//   let conversation=await Conversation.findOne({
-//    participants:{$all: [senderId, receiverId]},
+    const conversation=await Conversation.findOne({
+      participants:{$all:[senderId,userToChatId]}
+    }).populate("messages");
 
-//   })
+    if(!conversation){
+      return res.status(200).json([])
+    }
 
-//   if(!conversation){
-//    conversation=await Conversation.create({
-//     participants:[senderId,receiverId],
-//    })
-//   }
-//   const newMessage=new Message({
-//    senderId,
-//    receiverId,
-//    message,
-//   })
-
-//   if(newMessage){
-//    conversation.messages.push(newMessage._id)
-//   }
-//   res.status(201).json(newMessage);
-//  } catch (error) {
-//   console.log("error in send message controller",error.message)
-//   res.status(500).json({error:"internal server error"})
-//  }
-// }
+    const messages=conversation.messages
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in get message controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
