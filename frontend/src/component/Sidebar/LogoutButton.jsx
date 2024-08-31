92f394ef396f8ce02f4d567b06b12c443bcb236e
khushboo <palkhushboo2632@gmail.com>
2024-08-31 17:47:28 +0530

    create slidebar pages with logout button

---
+++ frontend/src/component/Sidebar/LogoutButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { RiLogoutBoxLine } from "react-icons/ri";
+function LogoutButton() {
+  return (
+    <div className='mt-auto'><RiLogoutBoxLine className='w-6 h-6 text-white cursor-pointer' /></div>
+  )
+}
+
+export default LogoutButton
