89a5a7a6950b8e95ca098cb012e222344ab49772
khushboo <palkhushboo2632@gmail.com>
2024-09-02 23:37:48 +0530

    display random emoji, username, and profilepic on UI

--- frontend/src/component/Sidebar/Conversation.jsx
+++ frontend/src/component/Sidebar/Conversation.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
-function Conversation() {
+function Conversation({conversations,lastIdx,emoji}) {
+  //console.log(conversations,lastIdx,emoji)
+
   return <>
   <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
     <div className='avatar online'>
      <div className='w-12 rounded-full'> 
-      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" alt='user-avatar'/>
+      <img src={conversations.profilePic} alt='user-avatar'/>
      </div>
     </div>
     <div className="flex flex-col flex-1">
      <div className='flex gap-3 justify-between'>
-      <p className='font-bold text-gray-200'>John Deo</p>
-       <span className='text-xl'>&#128525; </span>
+      <p className='font-bold text-gray-200'>{conversations.fullName}</p>
+       <span className='text-xl'>{emoji} </span>
       
      </div>
     </div>
   </div>
-  <div className='divider my-0 py-0 h-1'>
-
-  </div>
+  {!lastIdx && <div className='divider my-0 py-0 h-1'/>}
   </>
 }
 
--- frontend/src/component/Sidebar/Conversations.jsx
+++ frontend/src/component/Sidebar/Conversations.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Conversation from './Conversation'
 
+import useGetConversation from '../../hooks/useGetConversation';
+import { getRandomEmoji } from '../../utils/emoji';
+
+
 function Conversations() {
+ const {loading,conversation}= useGetConversation();
+ //console.log("conversation",conversation)
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-     <Conversation/>
-     <Conversation/>
-     <Conversation/>
-     <Conversation/>
+     {conversation.map((conversations,idx)=>(
+      <Conversation key={conversations._id}
+       conversations={conversations} 
+       emoji={getRandomEmoji()}
+       lastIdx={idx === conversation.length-1}
+       />))}
 
-
+      {loading? <span className='loading loading-spinner mx-auto'></span>:null}
     </div>
   )
 }
