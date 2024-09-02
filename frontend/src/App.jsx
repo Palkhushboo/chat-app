01cbd7befee10cd42ba32e256be77fc1207c1c07
khushboo <palkhushboo2632@gmail.com>
2024-09-02 13:59:30 +0530

    logout button functionality and navigation Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,19 @@ import './App.css'
 import Home from './pages/Home/Home'
 import Login from './pages/Login page/Login'
 import SignUp from './pages/sign Up page/SignUp'
-import { Route,Routes } from 'react-router-dom';
+import { Route,Routes ,Navigate} from 'react-router-dom';
 import {Toaster} from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext';
 
 function App() {
-  
+  const {authUser}=useAuthContext();
   return (
     <>
      <div className='p-4 h-screen flex items-center justify-center'>
      <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signUp' element={<SignUp/>}/>
+      <Route path='/' element={authUser?<Home/>:<Navigate to={"/login"}/>}/>
+      <Route path='/login' element={authUser?<Navigate to="/"/>:<Login/>}/>
+      <Route path='/signUp' element={authUser? <Navigate to='/'/>: <SignUp/>}/>
      </Routes>
      <Toaster/>
      </div>
--- frontend/src/component/Sidebar/LogoutButton.jsx
+++ frontend/src/component/Sidebar/LogoutButton.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+
 import { RiLogoutBoxLine } from "react-icons/ri";
+import useLogout from '../../hooks/useLogout';
 function LogoutButton() {
+
+  const {loading,logOut}=useLogout()
   return (
-    <div className='mt-auto'><RiLogoutBoxLine className='w-6 h-6 text-white cursor-pointer' /></div>
+    <div className='mt-auto'>
+      {!loading?(<RiLogoutBoxLine className='w-6 h-6 text-white cursor-pointer' onClick={logOut} />):(<span className='loading loading-spinner'></span>)}
+     
+      </div>
   )
 }
 
