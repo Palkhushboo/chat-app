cc7734b337e4feb32ed623bdffd33166c1fab451
khushboo <palkhushboo2632@gmail.com>
2024-09-11 20:48:19 +0530

    done

--- backend/db/connecttomongodbfile.js
+++ backend/db/connecttomongodbfile.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 const connectToMongoDB=async ()=>{
  try {
-   await mongoose.connect(process.env.MONGODB_URI);
+   await mongoose.connect(process.env.MONGODB_URI,{serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+    connectTimeoutMS: 30000});
    console.log("connected to mongodb");
  } catch (error) {
    console.log("error connecting to mongodb",error.message)
