f58816b98ecfafdea3e409a841d82798a72145d8
khushboo <palkhushboo2632@gmail.com>
2024-09-11 12:56:10 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -11,12 +11,18 @@ import {app, server} from './socket/socket.js'
 
 const PORT=process.env.PORT || 5000;
 
+const  __dirname=path.resolve();
 dotenv.config();
 app.use(cookiesParser())
 app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoute)
 app.use("/api/user",userRoute)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+ res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,()=>{
  connectToMongoDB();
